bdd7aa4c8c3cb2be1662f1a63fac4be3af8ce507
xxmuddlesxx <cybergoth@live.ie>
2015-05-30 15:23:16 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 application = {
     content = {
-        width = 320,
-        height = 480,
+        width = 640,
+        height = 860,
         scale = "letterbox", --will uniformly scale up content as much as possible,
         fps = 60
     },
